4e8dc30ebd70a033f9c3fa08730921184d2e163d
Razarx02 <specialforalem2mail.ru>
2019-10-19 11:51:53 +0600

    aaaaaaaa

---
+++ raid1e.go
@@ -0,0 +1,34 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func Raid1e(x, y int) {
+	if x < 0 || y < 0 {
+		return
+	}
+	y2 := y - 1
+	x2 := x - 1
+	for y1 := 0; y1 < y; y1++ {
+		if y1 != 0 && y1 != y2 {
+			for x1 := 0; x1 < x; x1++ {
+				if x1 == 0 || x1 == x2 {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+			z01.PrintRune(10)
+		} else {
+			for x1 := 0; x1 < x; x1++ {
+				if x1 == 0 && y1 == 0 || x1 == x2 && y1 == y2 && x != 1 && y != 1 {
+					z01.PrintRune('A')
+				} else if x1 == x2 && y1 == 0 || x1 == 0 && y1 == y2 {
+					z01.PrintRune('C')
+				} else {
+					z01.PrintRune('B')
+				}
+			}
+			z01.PrintRune(10)
+		}
+	}
+}
